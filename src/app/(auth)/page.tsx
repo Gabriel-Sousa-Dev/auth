a7200b225df7481ed7c5e972d79e5fc368c6f0d9
Gabriel-Sousa-Dev <gabrieldesousa.dev@gmail.com>
2025-04-25 14:57:45 -0300

    fix: remove no used import

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-import { GetModulesFromUser } from "@/actions/user/GetModulesFromUser";
 import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
